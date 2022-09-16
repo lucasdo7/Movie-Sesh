1f5f5641aad7f403a91df8f2d94342b0ce9d45dd
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-09-16 16:47:00 -0300

    wrote the routes for the lists and bookmarks controllers... lists#index ... bookmarks#destroy

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,7 @@
 class BookmarksController < ApplicationController
   def create
-    
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :movies, only: %i[index show search] do
-    resources :bookmarks, only: %i[create]
-  end
+  resources :movies, only: %i[index show search]
 
-  
+  resources :bookmarks, only: %i[create destroy]
+
+  resources :lists, only: %i[index]
 end
