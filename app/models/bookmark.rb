a7441978451cb0dc7baac8746239f9597d884bdd
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-09-21 16:48:38 -0300

    dropped the table list (cascade) from database

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,4 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
-  belongs_to :list
   validates :comment, :user_rating, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_20_185959) do
+ActiveRecord::Schema.define(version: 2022_09_21_194420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,6 @@ ActiveRecord::Schema.define(version: 2022_09_20_185959) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
     t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
-  end
-
-  create_table "lists", force: :cascade do |t|
-    t.string "genre"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "movies", force: :cascade do |t|
@@ -76,9 +70,7 @@ ActiveRecord::Schema.define(version: 2022_09_20_185959) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bookmarks", "lists"
   add_foreign_key "bookmarks", "movies"
-  add_foreign_key "movies", "lists"
   add_foreign_key "seshs", "movies"
   add_foreign_key "seshs", "users"
 end
