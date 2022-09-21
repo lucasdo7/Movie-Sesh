6e2c314fc65e173c6f906084b6c5afd47dda1c30
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-09-21 16:52:52 -0300

    removed comlumns from table movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_21_194420) do
+ActiveRecord::Schema.define(version: 2022_09_21_194946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,15 +30,12 @@ ActiveRecord::Schema.define(version: 2022_09_21_194420) do
     t.string "title"
     t.string "overview"
     t.float "rating_imdb"
-    t.string "movie_poster"
     t.integer "release_year"
     t.string "runtime"
     t.string "language"
-    t.string "youtrailler"
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "watched"
     t.index ["list_id"], name: "index_movies_on_list_id"
   end
 
