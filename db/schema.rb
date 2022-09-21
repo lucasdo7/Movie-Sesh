0e9ada5d90c5b3e389cd51f496f1e65267470a26
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-09-21 19:42:11 -0300

    added poster and genre columns to movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_21_195658) do
+ActiveRecord::Schema.define(version: 2022_09_21_224107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2022_09_21_195658) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "director"
+    t.string "genre"
+    t.string "poster"
     t.index ["list_id"], name: "index_movies_on_list_id"
   end
 
