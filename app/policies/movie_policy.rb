b5b6d056843746fe17cce21f4c03c7516cb714ee
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-09-21 20:33:17 -0300

    added some few style to movies index (cards)

--- app/policies/movie_policy.rb
+++ app/policies/movie_policy.rb
@@ -7,9 +7,9 @@ class MoviePolicy < ApplicationPolicy
       scope.all # isso vai executar o policy scope no controller index do modelo
       # scope.where(user: user) # disse q o user é o current user, ai n precisa fazer autorização 1 por 1
     end
-
-    def show
-      true
-    end
+  end
+  
+  def show?
+    true
   end
 end
